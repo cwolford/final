5142e514af739560f60813df02660abe136ed6ed
Claire Wolford <cwolford@andover.edu>
2014-02-18 00:34:01 -0500

    Added StaticPages controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,22 @@
 Final::Application.routes.draw do
+  resources :users
+  resources :orders
+  resources :sessions, only: [:new, :create, :destroy] 
+
+  get "orders/new"
+
+  root  'static_pages#home'
+  match '/signup', to: 'users#new', via: 'get'
+  match '/signin',  to: 'sessions#new',         via: 'get'
+  match '/signout', to: 'sessions#destroy',     via: 'delete'
+  match '/workorder',  to: 'orders#new',            via: 'get'
+  match '/home', to: 'static_pages#home', via: 'get'
+  match '/help',  to: 'static_pages#help',  via: 'get'
+  match '/about', to: 'static_pages#about', via: 'get'
+  match '/contact', to: 'static_pages#contact', via: 'get'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
